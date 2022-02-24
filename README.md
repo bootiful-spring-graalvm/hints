c0876aed4ab581a79beecab82d893a8f9745dd63
Josh Long <josh@joshlong.com>
2022-02-23 18:59:45 -0800

    graphql

--- README.md
+++ README.md
@@ -5,5 +5,66 @@ I want to incubate as many integrations as possible in the lead up to Spring Fra
 This project will serve Spring Native users first and then move over to Spring Framework 6 
 and Spring Boot 3 when it ships.
 
+## Usage 
 
+Add the dependency itself: 
 
+```xml 
+    <dependency>
+        <groupId>io.cloudnativejava</groupId>
+        <artifactId>hints</artifactId>
+        <version>0.0.1-SNAPSHOT</version>
+    </dependency>
+```
+
+Add my Artifactory repository to the build: 
+
+```xml 
+    <repositories>
+        <repository>
+            <snapshots>
+                <enabled>false</enabled>
+            </snapshots>
+            <id>central</id>
+            <name>libs-release</name>
+            <url>
+                https://cloudnativejava.jfrog.io/cloudnativejava/libs-release
+            </url>
+        </repository>
+        <repository>
+            <snapshots/>
+            <id>snapshots</id>
+            <name>libs-snapshot</name>
+            <url>
+                https://cloudnativejava.jfrog.io/cloudnativejava/libs-snapshot
+            </url>
+        </repository>
+        <repository>
+            <id>maven_central</id>
+            <name>Maven Central</name>
+            <url>https://repo.maven.apache.org/maven2/</url>
+        </repository>
+    </repositories>
+    <pluginRepositories>
+        <pluginRepository>
+            <snapshots>
+                <enabled>false</enabled>
+            </snapshots>
+            <id>central</id>
+            <name>plugins-release</name>
+            <url>
+                https://cloudnativejava.jfrog.io/cloudnativejava/plugins-release
+            </url>
+        </pluginRepository>
+        <pluginRepository>
+            <snapshots/>
+            <id>snapshots</id>
+            <name>plugins-snapshot</name>
+            <url>
+                https://cloudnativejava.jfrog.io/cloudnativejava/plugins-snapshot
+            </url>
+        </pluginRepository>
+    </pluginRepositories>
+```
+
+Then create a [Spring Native-aware project on the Spring Initializr](https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.6.3&packaging=jar&jvmVersion=17&groupId=com.example&artifactId=demo&name=demo&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.demo&dependencies=native) and enjoy!
---
+++ src/main/java/io/cloudnativejava/graphql/GraphQlNativeConfiguration.java
@@ -0,0 +1,59 @@
+package io.cloudnativejava.graphql;
+
+import graphql.GraphQL;
+import graphql.analysis.QueryVisitorFieldArgumentEnvironment;
+import graphql.analysis.QueryVisitorFieldArgumentInputValue;
+import graphql.execution.Execution;
+import graphql.execution.nextgen.result.RootExecutionResultNode;
+import graphql.language.*;
+import graphql.parser.ParserOptions;
+import graphql.schema.*;
+import graphql.schema.validation.SchemaValidationErrorCollector;
+import graphql.util.NodeAdapter;
+import graphql.util.NodeZipper;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ResourceHint;
+import org.springframework.nativex.hint.TypeHint;
+import org.springframework.nativex.type.NativeConfiguration;
+
+import java.util.List;
+
+import static org.springframework.nativex.hint.TypeAccess.*;
+
+/**
+ * Provides Spring Native hints to support using Spring GraphQL in a Spring Native
+ * application. The majority of these hints enable the graphiql web console
+ *
+ * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
+ */
+@NativeHint(trigger = Argument.class, types = @TypeHint(
+		typeNames = { "graphql.analysis.QueryTraversalContext", "graphql.schema.idl.SchemaParseOrder", },
+		types = { Argument.class, ArrayValue.class, Boolean.class, BooleanValue.class, DataFetchingEnvironment.class,
+				Directive.class, DirectiveDefinition.class, DirectiveLocation.class, Document.class,
+				EnumTypeDefinition.class, EnumTypeExtensionDefinition.class, EnumValue.class, EnumValueDefinition.class,
+				Execution.class, Field.class, FieldDefinition.class, FloatValue.class, FragmentDefinition.class,
+				FragmentSpread.class, GraphQL.class, GraphQLArgument.class, GraphQLCodeRegistry.Builder.class,
+				GraphQLDirective.class, GraphQLEnumType.class, GraphQLEnumValueDefinition.class,
+				GraphQLFieldDefinition.class, GraphQLInputObjectField.class, GraphQLInputObjectType.class,
+				GraphQLInterfaceType.class, GraphQLList.class, GraphQLNamedType.class, GraphQLNonNull.class,
+				GraphQLObjectType.class, GraphQLOutputType.class, GraphQLScalarType.class, GraphQLSchema.class,
+				GraphQLSchemaElement.class, GraphQLUnionType.class, ImplementingTypeDefinition.class,
+				InlineFragment.class, InputObjectTypeDefinition.class, InputObjectTypeExtensionDefinition.class,
+				InputValueDefinition.class, IntValue.class, InterfaceTypeDefinition.class,
+				InterfaceTypeExtensionDefinition.class, List.class, ListType.class, NodeAdapter.class, NodeZipper.class,
+				NonNullType.class, NullValue.class, ObjectField.class, ObjectTypeDefinition.class,
+				ObjectTypeExtensionDefinition.class, ObjectValue.class, OperationDefinition.class,
+				OperationTypeDefinition.class, ParserOptions.class, QueryVisitorFieldArgumentEnvironment.class,
+				QueryVisitorFieldArgumentInputValue.class, RootExecutionResultNode.class, ScalarTypeDefinition.class,
+				ScalarTypeExtensionDefinition.class, SchemaDefinition.class, SchemaExtensionDefinition.class,
+				SchemaValidationErrorCollector.class, SelectionSet.class, StringValue.class, TypeDefinition.class,
+				TypeName.class, UnionTypeDefinition.class, UnionTypeExtensionDefinition.class, VariableDefinition.class,
+				VariableReference.class, }, //
+		access = { //
+				PUBLIC_CLASSES, PUBLIC_CONSTRUCTORS, PUBLIC_FIELDS, PUBLIC_METHODS, DECLARED_CLASSES,
+				DECLARED_CONSTRUCTORS, DECLARED_FIELDS, DECLARED_METHODS } //
+))
+@ResourceHint(patterns = { "graphiql/index.html" })
+public class GraphQlNativeConfiguration implements NativeConfiguration {
+
+}

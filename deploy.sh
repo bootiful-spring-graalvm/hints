6ecc19b4e7d6a642654d6b15874d48d62cb6ce3f
Josh Long <josh@joshlong.com>
2022-02-25 10:55:57 -0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash 
+# if you want to publish a release, make sure
+# the version number in the project is a whole
+# integer, not a `-SNAPSHOT` build. otherwise
+# the build will be deployed to the staging repo
 echo "this script will prompt you for the GPG passphrase"
 export GPG_TTY=$(tty)
 mvn -DskipTests=true clean deploy 
